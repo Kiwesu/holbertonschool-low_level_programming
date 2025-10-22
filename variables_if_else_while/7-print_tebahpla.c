22488cf276108d10f3ea6f013d9efac7acc48fbc
Kiwesu <melchor.clarisa@outlook.com.au>
2025-10-22 06:40:15 +0000

    Added prints all the lowercase alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+  char ch = 'z';
+
+  while (ch >= 'a')
+  {
+    putchar(ch);
+    ch--;
+  }
+
+  putchar('\n');
+    return(0);
+}
