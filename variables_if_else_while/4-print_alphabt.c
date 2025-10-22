322ef614dcf0f420282a3afc7faeeceecafa6ceb
Kiwesu <melchor.clarisa@outlook.com.au>
2025-10-22 05:10:50 +0000

    Initial commit for printing alphabet except q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase, then followed by a new line.
+ * Return: Always 0
+ */
+int main(void)
+{
+  char ch = 'a';
+
+  /* Print lowercase alphabet */
+  if (ch != 'q' && ch !='e')
+    {
+      putchar(ch);
+      ch++;
+    }
+
+  putchar('\n');
+    return (0);
+}
