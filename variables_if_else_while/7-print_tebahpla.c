0d7d947f8e9f80c3cd6e996137f1c8bff1c67bee
Kiwesu <melchor.clarisa@outlook.com.au>
2025-10-22 06:44:25 +0000

    Modified spaces for betty compliance

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -9,11 +9,11 @@ int main(void)
   char ch = 'z';
 
   while (ch >= 'a')
-  {
-    putchar(ch);
-    ch--;
-  }
+    {
+      putchar(ch);
+      ch--;
+    }
 
   putchar('\n');
-    return(0);
+  return (0);
 }
